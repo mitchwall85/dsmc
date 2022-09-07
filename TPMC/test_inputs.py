fecdf04aeba4f6159885ac500e2eb613ba7dae1b
mitch <miwa6095@colorado.edu>
2022-09-06 20:09:15 -0600

    added function to calculate reflected velocity

---
+++ TPMC/test_inputs.py
@@ -0,0 +1,11 @@
+from weakref import ref
+from utilities import reflect_spectular
+import numpy as np
+
+c = np.array([1, 2, 3])
+wall = np.array([0, 0, 5])
+
+c_prime = reflect_spectular(c, wall)
+
+print(c_prime)
+print(c)
---
+++ TPMC/utilities.py
@@ -0,0 +1,17 @@
+import numpy as np
+
+def reflect_spectular(c: np.array, wall: np.array):
+    """calculate the reflected velocity for a specular wall impact
+
+    Args:
+        c (np.array): incomming velocity
+        wall (np.array): wall normal vector
+    """
+    # ensure wall vector is a unit vector
+    wall = wall/np.linalg.norm(wall)
+
+    c_n = np.dot(c,wall)*wall # normal component to wall
+    c_p = c - c_n # perpendicular component to wall
+    c_prime = c_p - c_n # flip normal component
+
+    return c_prime
