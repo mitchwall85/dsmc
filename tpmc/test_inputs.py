b004dc9d1ac1a7603ed3e082598acdfda1f6f9fe
mitch <miwa6095@colorado.edu>
2022-09-26 19:25:05 -0600

    working test for circular position generation function

--- tpmc/test_inputs.py
+++ tpmc/test_inputs.py
@@ -2,7 +2,7 @@ import numpy as np
 
 from particle import PARTICLE
 
-from utilities import read_stl, gen_velocity
+from utilities import read_stl, gen_velocity, gen_posn
 
 import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
@@ -13,25 +13,21 @@ wall = np.array([0, 0, 5])  # wall normal vector
 print('velocity reflection test')
 particle = PARTICLE(1, 1, c, np.array([1,1,1]), 0, )
 print(particle.vel)
-particle.reflect_specular(wall)
-print(particle.vel)
-
-print(c)
 
 
-# read in an STL file
-print('read mesh test')
-file_name = r"../../geometry/cylinder_d2mm_l20mm.stl"
-your_mesh = read_stl(file_name)
-points = np.reshape(your_mesh.points,(np.shape(your_mesh.points)[0]*3,3))
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(points[:,0],points[:,1],points[:,2])
+
+# test random inlet generation
+TUBE_D = 0.002
+INLET_GRID_NAME = r"../../geometry/cylinder_d2mm_l20mm_inlet.stl"
+inlet_grid = read_stl(INLET_GRID_NAME)
+r = np.zeros(3)
+for i in np.arange(1,2000):
+    r = np.vstack([r,gen_posn(TUBE_D)])   
+
+plt.scatter(r[:,1],r[:,2])
+plt.ylim([-1.1*TUBE_D/2, 1.1*TUBE_D/2])
+plt.xlim([-1.1*TUBE_D/2, 1.1*TUBE_D/2])
 plt.show()
-
-c = gen_velocity(np.array([1e-8,2e-8,3e-8]), 100000, 1)
-print('velocity generation test')
-print(c)
 
 
 
